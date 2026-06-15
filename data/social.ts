366850b5c8c3a1f3bd56a70488237d08736bfb65
hamidali54 <hamidali54@gmail.com>
2026-06-15 18:54:02 +0500

    clean repo

--- data/social.ts
+++
@@ -1,6 +0,0 @@
-export const socialLinks = {
-  email: "mailto:hanzla.tauqeer123@gmail.com",
-  linkedin: "https://www.linkedin.com/in/1hanzla100/",
-  github: "https://github.com/1hanzla100",
-  instagram: "https://www.instagram.com/hanzla_tauqeer",
-};
